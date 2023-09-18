616995fbdbf3e28d970a6af912b0eed1cfbd2342
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-18 09:39:57 +0600

    Added all colors.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="container mx-auto max-sm:px-5 mt-12 mb-8">
-      <h1 className="text-center text-4xl font-bold">Course Registration</h1>
+      <h1 className="text-center text-4xl font-bold text-heading">Course Registration</h1>
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ export default {
       fontFamily: {
         inter: "'Inter', sans-serif",
       },
+      colors: {
+        heading: '#1C1B1B',
+        body: '#1C1B1B99',
+        theme: '#2F80ED',
+      },
     },
   },
   plugins: [],
