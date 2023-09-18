f2a30b43671ddbf9881276bcdcf814b0fb7b4d25
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-18 09:28:23 +0600

    Added Header components.

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="container mx-auto max-sm:px-5 mt-12 mb-8">
+      <h1 className="text-center text-4xl font-bold">Course Registration</h1>
+    </header>
+  );
+};
+
+export default Header;
